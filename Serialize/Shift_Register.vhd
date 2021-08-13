f7ba0585a2228679827000e2c92f11502195296d
Tejasva Agarwal <tejasva@bu.edu>
2021-08-13 13:06:23 -0400

    -

--- Serialize/Shift_Register.vhd
+++ Serialize/Shift_Register.vhd
@@ -9,43 +9,50 @@ use IEEE.numeric_std.all;
 entity Shift_Register is
 
   port (
-    sclk        : in std_logic;                         -- Serial clock.
-    start       : in std_logic;                         -- Enables shifting.
-    dir         : in std_logic;                         -- Specifies direction.
-    count       : in std_logic_vector(4 downto 0);      -- Specifies no. of bits.
-    data        : in std_logic_vector(31 downto 0);     -- Parallel data.
-    sdata       : out std_logic);                       -- Serial data.
+    clock : in  std_logic;              -- System clock.
+    reset : in  std_logic;              -- Synchronous active high reset.  
+    s_en  : in  std_logic;              -- Serial clock.
+    start : in  std_logic;              -- Enables shifting.
+    dir   : in  std_logic;              -- Specifies direction.
+    count : in  std_logic_vector(4 downto 0);   -- Specifies no. of bits.
+    data  : in  std_logic_vector(31 downto 0);  -- Parallel data.
+    sdata : out std_logic);             -- Serial data.
 
 end Shift_Register;
 
 
 architecture Shifting of Shift_Register is
 
-  signal count_int : integer range 0 to 31;
-  signal cycle     : integer range 0 to 31;
-  
-  
+  signal count_int       : integer range 0 to 31;  -- Integer value of count vector.
+  signal cycle           : integer range 0 to 31;  -- Ascending or descending.                  
+  signal registered_data : std_logic_vector(31 downto 0);  -- Temporary vector.
+
 begin
 
-  process(sclk)
+  process(clock)
   begin
 
-    count_int <= to_integer(unsigned(count));
-
-    if (falling_edge(sclk))then
+    if (falling_edge(clock))then
       if (dir = '0')then
+        count_int <= to_integer(unsigned(count));
         if (start = '1')then
-          cycle <= 0;
+          registered_data <= data;
+          cycle           <= 0;
         elsif (cycle < count_int)then
           cycle <= cycle + 1;
         end if;
       elsif (dir = '1')then
+        count_int <= to_integer(unsigned(count));
         if (start = '1')then
-          cycle <= count_int;
+          registered_data <= data;
+          cycle           <= count_int;
         elsif (cycle > 0)then
           cycle <= cycle - 1;
         end if;
       end if;
+    end if;
+
+    if (falling_edge(s_en))then
       sdata <= data(cycle);
     end if;
 
--- Serialize/clkdiv.vhd
+++ Serialize/clkdiv.vhd
@@ -5,41 +5,41 @@ use ieee.numeric_std.all;
 
 entity clkdiv is
 
-    generic (divider : integer := 5); -- Clock time period /2
-    port (
-        clock : in std_logic;  -- The input 100MHZ clock.
-        reset : in std_logic;  -- Synchronous active high reset.
-        stdclk : out std_logic -- Standard Clock with 1 HZ frequency.
+  generic (divider : integer := 5);     -- Clock time period /2
+  port (
+    clock  : in  std_logic;             -- The input 100MHZ clock.
+    reset  : in  std_logic;             -- Synchronous active high reset.
+    s_enable : out std_logic            -- Standard Clock with 1 HZ frequency.
     );
 end clkdiv;
 
 architecture rtl of clkdiv is
 
-    signal clk_count : integer range 0 to divider;  -- Clock Count to count till the input clock reaches the divider.
-    signal temp : std_logic; -- Temporary variable to store the standard clock value.
+  signal clk_count : integer range 0 to divider;  -- Clock Count to count till the input clock reaches the divider.
+  signal temp      : std_logic;  -- Temporary variable to store the standard clock value.
 
 begin
-    
-    process (clock)
-    begin
-    
-        if (rising_edge(clock)) then -- Everything is Synchronous.
-            if (reset = '1') then 
-                temp <= '0';            
-                stdclk <= '0';
-                clk_count <= 0 ;
-                else          
-                    if (clk_count = divider-1 and temp = '0') then 
-                        temp <= '1'; 
-                        clk_count <= 0 ;
-                    elsif (temp = '1') then 
-                        temp <= '0';
-                    else
-                        clk_count <= clk_count + 1;    
-                    end if;   
-                    stdclk <= temp;
-            end if;    
+
+  process (clock)
+  begin
+
+    if (rising_edge(clock)) then        -- Everything is Synchronous.
+      if (reset = '1') then
+        temp      <= '0';
+        s_enable    <= '0';
+        clk_count <= 0;
+      else
+        if (clk_count = divider-1 and temp = '0') then
+          temp      <= '1';
+          clk_count <= 0;
+        elsif (temp = '1') then
+          temp <= '0';
+        else
+          clk_count <= clk_count + 1;
         end if;
-    end process;
-    
+        s_enable <= temp;
+      end if;
+    end if;
+  end process;
+
 end architecture;
